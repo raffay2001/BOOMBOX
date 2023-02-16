09e397553a22e1f8b590f9abd81455d44241cac0
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-17 01:24:53 +0500

    add: hero banner section and layout component

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,16 @@ const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products.</h2>
         <p>Speakers of many variations.</p>
       </div>
-      <div className="products-container">{products?.map((product) => product.name)}</div>
-      <FooterBanner />
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData.length && bannerData[0]} />
     </>
   )
 }
