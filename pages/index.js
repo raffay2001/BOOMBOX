c529af3ee4f4048d49b3c8eb2fe2b55957c68f5b
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-17 00:41:48 +0500

    add: hero banner section

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
-export default function Home() {
+import React from "react"
+import { Product, FooterBanner, HeroBanner } from "../components"
+import { client } from "@/lib/client"
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <h1>Hello from Next</h1>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
+      <div className="products-heading">
+        <h2>Best Selling Products.</h2>
+        <p>Speakers of many variations.</p>
+      </div>
+      <div className="products-container">{products?.map((product) => product.name)}</div>
+      <FooterBanner />
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]'
+  const products = await client.fetch(query)
+  const bannerQuery = '*[_type=="banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+  return {
+    props: { products, bannerData },
+  }
+}
+
+export default Home
